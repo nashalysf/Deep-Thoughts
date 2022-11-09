3ad9b95a3db245face886d7431c48089ab63edcf
nashalysf <nashalysf@gmail.com>
2022-11-09 13:04:32 -0400

    heroku staging

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "https://git.heroku.com/lit-harbor-12267.git",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/deepthoughtscluster",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
